db17fa95fd6105b64f2bbefa0981f2d37cb68e06
Peter Marriott <peter.marriott@catalystcomputing.co.uk>
2019-03-21 08:51:17 +0000

    Basic tests

--- sam/sam-function/tests/unit/test_handler.py
+++ sam/sam-function/tests/unit/test_handler.py
@@ -1,73 +1,61 @@
 import json
+import unittest
 
-import pytest
+from call_by_sns import app
 
-from hello_world import app
-
-
-@pytest.fixture()
-def apigw_event():
-    """ Generates API GW Event"""
+def sns_event():
+    """ Generates SNS Event"""
 
     return {
-        "body": '{ "test": "body"}',
-        "resource": "/{proxy+}",
-        "requestContext": {
-            "resourceId": "123456",
-            "apiId": "1234567890",
-            "resourcePath": "/{proxy+}",
-            "httpMethod": "POST",
-            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
-            "accountId": "123456789012",
-            "identity": {
-                "apiKey": "",
-                "userArn": "",
-                "cognitoAuthenticationType": "",
-                "caller": "",
-                "userAgent": "Custom User Agent String",
-                "user": "",
-                "cognitoIdentityPoolId": "",
-                "cognitoIdentityId": "",
-                "cognitoAuthenticationProvider": "",
-                "sourceIp": "127.0.0.1",
-                "accountId": "",
+    "Records": [
+      {
+        "EventSource": "aws:sns",
+        "EventVersion": "1.0",
+        "EventSubscriptionArn": "arn:aws:sns:us-east-1::SNSTopic",
+        "Sns": {
+          "Type": "Notification",
+          "MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
+          "TopicArn": "arn:aws:sns:us-east-1:123456789012:SNSTopic",
+          "Subject": "example subject",
+          "Message": "Hello from SNS",
+          "Timestamp": "1970-01-01T00:00:00.000Z",
+          "SignatureVersion": "1",
+          "Signature": "EXAMPLE",
+          "SigningCertUrl": "EXAMPLE",
+          "UnsubscribeUrl": "EXAMPLE",
+          "MessageAttributes": {
+            "Test": {
+              "Type": "String",
+              "Value": "TestString"
             },
-            "stage": "prod",
-        },
-        "queryStringParameters": {"foo": "bar"},
-        "headers": {
-            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
-            "Accept-Language": "en-US,en;q=0.8",
-            "CloudFront-Is-Desktop-Viewer": "true",
-            "CloudFront-Is-SmartTV-Viewer": "false",
-            "CloudFront-Is-Mobile-Viewer": "false",
-            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
-            "CloudFront-Viewer-Country": "US",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Upgrade-Insecure-Requests": "1",
-            "X-Forwarded-Port": "443",
-            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
-            "X-Forwarded-Proto": "https",
-            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
-            "CloudFront-Is-Tablet-Viewer": "false",
-            "Cache-Control": "max-age=0",
-            "User-Agent": "Custom User Agent String",
-            "CloudFront-Forwarded-Proto": "https",
-            "Accept-Encoding": "gzip, deflate, sdch",
-        },
-        "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "POST",
-        "stageVariables": {"baz": "qux"},
-        "path": "/examplepath",
-    }
+            "TestBinary": {
+              "Type": "Binary",
+              "Value": "TestBinary"
+            }
+          }
+        }
+      }
+    ]
+  }
 
+class HelloSNS(unittest.TestCase):
 
-def test_lambda_handler(apigw_event, mocker):
+    def test_lambda_handler_valid_sns_returns_200(self):
+        event = sns_event()
+        ret = app.lambda_handler(event, "")
 
-    ret = app.lambda_handler(apigw_event, "")
-    data = json.loads(ret["body"])
+        assert ret["statusCode"] == 200
 
-    assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
-    # assert "location" in data.dict_keys()
+    def test_lambda_handler_valid_sns_has_message(self):
+        event = sns_event()
+        ret = app.lambda_handler(event, "")
+
+        assert "message" in ret["body"]
+
+    def test_lambda_handler_valid_sns_has_message_from_event(self):
+        event = sns_event()
+        ret = app.lambda_handler(event, "")
+        data = json.loads(ret["body"])
+
+        assert "message" in ret["body"]
+        assert data["message"] == "Hello from SNS"
